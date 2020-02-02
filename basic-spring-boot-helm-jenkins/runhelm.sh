a8b5df51d503922c029322d76588b12fa8cb27f5
Thomas Peters-Hall <ramius345@gmail.com>
2020-02-02 11:03:38 -0600

    auto scripting around instantiating projects

---
+++ basic-spring-boot-helm-jenkins/runhelm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+PROJECTNAME=$1
+GITREPO=$2
+BRANCH=$3
+JENKINS_PROJECT=$4
+DEV_PROJECT=$5
+
+
+echo "Project: $PROJECTNAME"
+echo "Git Repo: $GITREPO"
+echo "Branch: $BRANCH"
+echo "Jenkins Pipeline Project: $JENKINS_PROJECT"
+echo "Dev Project: $DEV_PROJECT"
+echo ""
+echo "Your Jenkinsfile (saved to Jenkinsfile, copy to your git project!):"
+cat Jenkinsfile.template | \
+    sed "s/<PROJECT_NAME>/${PROJECTNAME}/" | \
+    sed "s/<JENKINS_PROJECT>/${JENKINS_PROJECT}/" | \
+    sed "s/<DEV_PROJECT>/${DEV_PROJECT}/" | \
+    tee Jenkinsfile 
+    
+
+oc new-project $JENKINS_PROJECT
+helm install \
+     --name-template jenkins  \
+     --set projectname=$PROJECTNAME \
+     --set git.repo=$GITREPO \
+     --set git.ref=$BRANCH \
+     pipeline-chart
+
+
+oc new-project $DEV_PROJECT
+helm install \
+     --name-template $PROJECTNAME \
+     --set projectname=$PROJECTNAME \
+     --set jenkins.pipeline_project=$JENKINS_PROJECT \
+     --set git.repo=$GITREPO \
+     --set git.ref=$BRANCH \
+     dev-project-chart
+
+
