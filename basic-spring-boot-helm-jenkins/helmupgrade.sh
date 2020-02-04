bf05d7f5fa7c14d2f30f152dfadf2d1494d8b04a
Thomas Peters-Hall <ramius345@gmail.com>
2020-02-03 23:10:47 -0600

    added httpd setup

---
+++ basic-spring-boot-helm-jenkins/helmupgrade.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PROJECTNAME=$1
+GITREPO=$2
+BRANCH=$3
+JENKINS_PROJECT=$4
+DEV_PROJECT=$5
+
+
+echo "Project: $PROJECTNAME"
+echo "Git Repo: $GITREPO"
+echo "Branch: $BRANCH"
+echo "Jenkins Pipeline Project: $JENKINS_PROJECT"
+echo "Dev Project: $DEV_PROJECT"
+echo ""
+echo "Your Jenkinsfile (saved to Jenkinsfile, copy to your git project!):"
+
+oc project $JENKINS_PROJECT
+helm upgrade \
+     --set projectname=$PROJECTNAME \
+     --set git.repo=$GITREPO \
+     --set git.ref=$BRANCH \
+     jenkins pipeline-chart
+
+
+oc project $DEV_PROJECT
+helm upgrade \
+     --set projectname=$PROJECTNAME \
+     --set jenkins.pipeline_project=$JENKINS_PROJECT \
+     --set git.repo=$GITREPO \
+     --set git.ref=$BRANCH \
+     $PROJECTNAME dev-project-chart
+
+
